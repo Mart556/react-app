87a5f5c86fd2ac7a80240e9d2b5a810edc3ddfc9
Mart Haamer <marthaamer123@gmail.com>
2025-01-23 12:21:10 +0200

    Revert "Added expenses items and edited css"
    This reverts commit 7ab716120a514fb0d5c06f268b29d96aaa84bef5.

--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./ExpensesItem.css";
 
-const ExpensesItem = (props) => {
+const ExpensesItem = () => {
   return (
     <div className="expense-item">
-      <h2>{props.date}</h2>
-      <hr />
+      <div>Date</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.price}$</div>
+        <h2>Item</h2>
+        <div className="expense-item__price">Price</div>
       </div>
     </div>
   );
